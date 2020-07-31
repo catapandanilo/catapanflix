4df248d8dd76da1c8c65df1afd30d6ea44648b2a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-31 14:54:46 -0300

    add cadastro video route

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import CadastroVideo from './pages/cadastro/Video'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component= { App } exact />
-      <Route component= { () => (<div>Page 404</div>) } />
+      <Route path="/cadastro/video" component= { CadastroVideo } />
+      <Route component= { () => (<div>Página 404</div>) } />
     </Switch>
   </BrowserRouter>,
 
