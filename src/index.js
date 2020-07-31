d62426676f95888416aec41c6360f3c7c7785a1e
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-31 16:30:04 -0300

    add CadastroCategoria

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import './index.css';
 
 import Home from './pages/Home'
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component= { Home } exact />
       <Route path="/cadastro/video" component= { CadastroVideo } />
+      <Route path="/cadastro/categoria" component= { CadastroCategoria } />
       <Route component= { () => (<div>Página 404</div>) } />
     </Switch>
   </BrowserRouter>,
