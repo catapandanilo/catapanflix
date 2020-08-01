eea42b77d71056885cd570612234f9f576783be4
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-31 22:02:42 -0300

    add userState, setNomeDaCategoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault'
 import { Link } from 'react-router-dom'
 
 function CadastroCategoria () {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes')
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1> Cadastro de Categoria: { nomeDaCategoria } </h1>
 
-      <form>
+      <form onSubmit = { function handleSubmit (infoDoEvento) {
+        infoDoEvento.preventDefault()
+      }}>
         <label>
           Nome da Categoria:
           <input
-            type="text"
+            type = "text"
+            value = { nomeDaCategoria }
+            onChange = { (infoDoEvento) => {
+              setNomeDaCategoria (infoDoEvento.target.value)
+            }}
           />
         </label>
 
