e5365d645c32e6777e777214cff83444966fc8cf
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 08:42:43 -0300

    add method getAll categories

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,17 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`;
 
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (respostaServidor) => {
+      if (respostaServidor.ok) {
+        const respostaServidorJson = await respostaServidor.json();
+        return respostaServidorJson;
+      }
+      throw new Error('Não foi possível conectar com o servidor backend');
+    });
+}
+
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaServidor) => {
@@ -14,5 +25,6 @@ function getAllWithVideos() {
 }
 
 export default {
+  getAll,
   getAllWithVideos,
 };
