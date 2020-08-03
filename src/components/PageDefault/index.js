bd386fab30b5ec20c03a03cafe60e7bf76c0dd8e
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 00:15:35 -0300

    refactor home index using useEffect and PageDefault

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Menu from '../Menu'
-import Footer from '../Footer'
-import styled from 'styled-components'
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Menu from '../Menu';
+import Footer from '../Footer';
 
 const Main = styled.main`
   background-color: var(--black);
@@ -9,19 +9,22 @@ const Main = styled.main`
   flex: 1;
   padding-top: 50px;  
   padding-left: 5%;  
-  padding-right: 5%;  
-`
+  padding-right: 5%;
+  ${({ paddingAll }) => css`
+    padding: ${paddingAll};
+  `}
+`;
 
-function PageDefault ({ children }) {
+function PageDefault({ children, paddingAll }) {
   return (
     <>
       <Menu />
-        <Main>
-          { children }
-        </Main>
+      <Main paddingAll={paddingAll}>
+        { children }
+      </Main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default PageDefault
+export default PageDefault;
