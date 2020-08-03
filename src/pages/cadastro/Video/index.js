971d7bbe8087bf5b8c49779ea818c0d180810232
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 00:28:33 -0300

    refactor create video, add field and use useHistory

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
-function CadastroVideo () {
+function CadastroVideo() {
+  const history = useHistory();
+  const { handleChange, values } = useForm({
+
+  });
+
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        history.push('/');
+      }}
+      >
+        <FormField
+          label="Título do Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
 
       <Link to="/cadastro/categoria">
         Cadastrar Categoria
       </Link>
     </PageDefault>
-  )
+  );
 }
 
-export default CadastroVideo
+export default CadastroVideo;
